6b323140c9d27d9a17682774c72ab4e8af4a6f40
MissGhasempour <romi.ghasem@gmail.com>
2025-04-03 18:57:50 +0330

    header component created

---
+++ src/app/components/header/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header () {
+    return <div>
+        <div className="flex">
+            <div>Products</div>
+            <div>Brands</div>
+            <div>About Us</div>
+            <div>Contact Us</div>
+        </div>
+    </div>
+}
