66cc3cd1363a5b1e7763fc951e36a3aa4393aa1e
MissGhasempour <romi.ghasem@gmail.com>
2025-04-03 19:16:06 +0330

    navbar created in header

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -1,10 +1,12 @@
-export default function Header () {
-    return <div>
-        <div className="flex">
-            <div>Products</div>
-            <div>Brands</div>
-            <div>About Us</div>
-            <div>Contact Us</div>
-        </div>
+export default function Header() {
+  return (
+    <div>
+      <div className="flex relative left-100 m-2">
+        <div className=" border-1 w-30 p-2 text-center">Products</div>
+        <div className=" border-1 w-30 p-2 text-center">Brands</div>
+        <div className=" border-1 w-30 p-2 text-center">About Us</div>
+        <div className=" border-1 w-30 p-2 text-center">Contact Us</div>
+      </div>
     </div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import Header from "./components/header/Header";
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <div>grandeur</div>
-      <Header />
+    <div className="flex">
+      <div className="m-2">grandeur</div>
+      <Header/>
     </div>
   );
 }
